645ec1bacbf9982577c3a7c14076140621250d75
Shadab Zafar <dufferzafar0@gmail.com>
2014-06-18 15:01:27 +0530

    Removed templating tasks from gulpfile
    ...as they are no longer required.

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,13 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     replace = require('gulp-replace'),
     minifycss = require('gulp-minify-css'),
-    fileinclude = require('gulp-file-include'),
     less = require('gulp-less');
 
 // Create a webserver
 gulp.task('webserver', function() {
   connect.server({
     livereload: true,
-    root: ['tmp']
+    root: ['.']
   });
 });
 
@@ -21,44 +20,16 @@ gulp.task('webserver', function() {
 gulp.task('less', function() {
   return gulp.src('static/less/styles.less')
       .pipe(less())
-      .pipe(gulp.dest('tmp/static/css'))
-      .pipe(connect.reload());
-});
-
-// Copy js files
-gulp.task('js', function() {
-  gulp.src('static/js/*.js')
-    .pipe(gulp.dest('tmp/static/js'))
-    .pipe(connect.reload());
-});
-
-// Include templates in html files
-gulp.task('template', function() {
-  return gulp.src(['*.html'])
-    .pipe(fileinclude({
-        prefix: '@@',
-        basepath: '@file'
-      }))
-    .pipe(gulp.dest('tmp'))
-    .pipe(connect.reload());
+      .pipe(gulp.dest('static/css'))
 });
 
 // Run tasks when files in 'static' directory change
 gulp.task('watch', function() {
   gulp.watch('static/less/**', ['less']);
-  gulp.watch('static/js/*.js', ['js']);
-  gulp.watch('*.html', ['template']);
-  gulp.watch('templates/*.html', ['template']);
-})
-
-// Copy files from the static directory to the tmp dir
-gulp.task('copy-tmp', function() {
-  gulp.src(['*.html', './static/**'], {base: './'})
-      .pipe(gulp.dest('tmp'));
 });
 
 // Task for development
-gulp.task('dev', ['copy-tmp', 'template', 'less', 'watch', 'webserver'], function() {
+gulp.task('dev', ['less', 'watch', 'webserver'], function() {
   var options = {
     url: "http://localhost:8080",
     app: "firefox"
@@ -77,8 +48,8 @@ gulp.task('default', ['dev']);
 var buildDir = './_gh-pages';
 
 // Copy files to push to server
-gulp.task('copy-build', ['copy-tmp', 'template'], function() {
-  gulp.src(['tmp/*.html', './tmp/**'], {base: './tmp'})
+gulp.task('copy-build', function() {
+  gulp.src(['templates/*.html', './static/**'], {base: './'})
       .pipe(gulp.dest(buildDir));
 });
 
