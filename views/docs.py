7cbb049990720a9f9e0f2bfe91597050dd5ab3aa
Shadab Zafar <dufferzafar0@gmail.com>
2014-08-05 19:06:00 +0530

    A general endpoint for all doc templates

--- views/docs.py
+++ views/docs.py
@@ -1,8 +1,18 @@
 from app import app
-from flask import render_template
+from flask import (
+    render_template,
+    abort
+)
 
+import os
 
 @app.route('/docs/')
-@app.route('/docs/basics/')
-def show_docs():
+def show_basics():
     return render_template('docs/basics.html')
+
+@app.route('/docs/<string:page>/')
+def show_pages(page):
+    if os.path.isfile('templates/docs/' + page + '.html'):
+        return render_template('docs/' + page + '.html')
+    else:
+        abort(404)
