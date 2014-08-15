22d3c418891e0abc8608b886cce5a041b5be60e8
Shadab Zafar <dufferzafar0@gmail.com>
2014-08-15 10:12:11 +0530

    Fixed the plugins view
    Now uses the already existing json data rather than fetching it from
    Github.
    
    The table has now been made fixed width so, that arbitrary descriptions
    don't mess it up.

--- views/plugins.py
+++ views/plugins.py
@@ -4,10 +4,8 @@ from flask import render_template
 import json
 from urllib import urlopen
 
-
 @app.route('/plugins/')
 def show_plugins():
-    url = "https://raw.githubusercontent.com/dufferzafar" \
-          "/picard-plugins/master/plugins.json"
-    plugins = json.loads(urlopen(url).read().decode("utf-8"))
+    with open('plugins.json', "r") as plugin_file:
+        plugins = json.loads(plugin_file.read().decode("utf-8"))
     return render_template('plugins.html', plugins=plugins['plugins'])
