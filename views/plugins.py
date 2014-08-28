cb2740b04ebd0f370fda833b96033689d4a71e44
Michael Wiencek <mwtuea@gmail.com>
2014-08-28 18:49:01 -0500

    Remove hardcoded plugins.json reference

--- views/plugins.py
+++ views/plugins.py
@@ -1,11 +1,14 @@
-from app import app
-from flask import render_template
-
+import os
 import json
 from urllib import urlopen
+from flask import render_template
+from app import app
+from config import PLUGINS_REPOSITORY
+
+PLUGINS_JSON_FILE = os.path.join(PLUGINS_REPOSITORY, "plugins.json")
 
 @app.route('/plugins/')
 def show_plugins():
-    with open('plugins.json', "r") as plugin_file:
-        plugins = json.loads(plugin_file.read().decode("utf-8"))
+    with open(PLUGINS_JSON_FILE, "r") as fp:
+        plugins = json.loads(fp.read().decode("utf-8"))
     return render_template('plugins.html', plugins=plugins['plugins'])
