eaabb34bec7cacb011e1a94810c6b5365610e1f0
Laurent Monin <zas@norz.org>
2014-09-12 00:30:41 +0200

    Use caching instead of loading changelog on all requests

--- website/default_config.py
+++ website/default_config.py
@@ -1,3 +1,6 @@
 PLUGINS_REPOSITORY = "/home/picard/picard-plugins"
+CHANGELOG_URL = "https://raw.githubusercontent.com/musicbrainz/picard/master/NEWS.txt"
+CHANGELOG_CACHE_TIMEOUT = 5*60
+
 SERVER_HOSTNAME = "0.0.0.0"
 SERVER_PORT = 6060
--- website/frontend/views/changelog.py
+++ website/frontend/views/changelog.py
@@ -1,10 +1,20 @@
-from flask import Blueprint, render_template
+from urllib import urlopen
+from flask import current_app, Blueprint, render_template
 
 
 changelog_bp = Blueprint('changelog', __name__)
 
+
+def load_changelog(app):
+    key = 'changelog_data'
+    data = app.cache.get(key)
+    if data is None:
+        url = app.config['CHANGELOG_URL']
+        data = urlopen(url).read().decode("utf-8-sig").splitlines()
+        app.cache.set(key, data, timeout=app.config['CHANGELOG_CACHE_TIMEOUT'])
+    return data
+
+
 @changelog_bp.route('/')
 def show_changelog():
-    url = "https://raw.githubusercontent.com/musicbrainz/picard/master/NEWS.txt"
-    data = urlopen(url).read().decode("utf-8-sig")
-    return render_template('changelog.html', lines=data.splitlines())
+    return render_template('changelog.html', lines=load_changelog(current_app))
