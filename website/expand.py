5460eda35e78c2c9ad9b0f6ee40cdcd920ec0c8a
Laurent Monin <zas@norz.org>
2014-09-16 22:12:01 +0200

    Minor fixes in expand()
    - remove unneeded test (var is in args since it is used to build the regex)
    - handle the case where default_attribute is an empty string

--- website/expand.py
+++ website/expand.py
@@ -16,18 +16,19 @@ def expand(string, args, tag='a', default_attribute='href'):
             final_text = args[text]
         else:
             final_text = text
-        if var in args:
-            if isinstance(args[var], dict):
-                d = args[var]
-            else:
-                d = {default_attribute: args[var]}
-            attribs = ' '.join(["%s=\"%s\"" % (k, encode_entities(d[k])) for k
-                                in sorted(d.keys())])
-            if attribs:
-                attribs = ' ' + attribs
-            return '<%s%s>%s</%s>' % (tag, attribs, final_text, tag)
+
+        if isinstance(args[var], dict):
+            d = args[var]
         else:
-            return '{%s|%s}' % (var, text)
+            if default_attribute:
+                d = {default_attribute: args[var]}
+            else:
+                d = {}
+        attribs = ' '.join(["%s=\"%s\"" % (k, encode_entities(d[k])) for k
+                            in sorted(d.keys())])
+        if attribs:
+            attribs = ' ' + attribs
+        return '<%s%s>%s</%s>' % (tag, attribs, final_text, tag)
 
     def simple_expr(match):
         var = match.group(1)
