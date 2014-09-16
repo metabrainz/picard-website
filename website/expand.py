f3dc6f27dec9da17014aed0a3494c6f696afb2ed
Laurent Monin <zas@norz.org>
2014-09-16 22:12:40 +0200

    Remove unused code

--- website/expand.py
+++ website/expand.py
@@ -45,31 +45,3 @@ def expand(string, args, tag='a', default_attribute='href'):
     string = r2.sub(simple_expr, string)
 
     return string
-
-
-if __name__ == "__main__":
-    d = {
-        'url': 'http://un.site.com/path?id=param',
-        'truc': 'page#uné<""mot',
-        'url3': {'href': 'http://deux.site.com/path?id=param',
-                 'class': 'theclass',
-                 'title': 'TheTitle'},
-        'is': 'was',
-        'xxxx': 'zzzz'
-    }
-
-    s = u'{this} {is} an {url|URLTITLE1} xx {truc|xxxx} é {no|match}xxxxxx {url3|URLTITLE2}'
-    print 'DICT: ', repr(d)
-    print 'FROM: ', s
-    print 'TO  : ', vars_expand(s, d)
-
-    d = {
-        'span': {'id': 'xxx', 'class': 'zzzz'},
-        'is': 'was',
-        'test': 'id23',
-    }
-
-    s = u'{this} {is} an {span|texte} {test|} {url3|URLTITLE2}'
-    print 'DICT: ', repr(d)
-    print 'FROM: ', s
-    print 'TO  : ', vars_expand(s, d, tag='span', default_attribute='id')
