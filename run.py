52906467ed22807d97777e611143c7dff2e1e391
Michael Wiencek <mwtuea@gmail.com>
2014-10-20 19:24:10 -0500

    Export app in run.py for uwsgi

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 from werkzeug.serving import run_simple
 from website.frontend import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     run_simple(app.config['SERVER_HOSTNAME'], app.config['SERVER_PORT'], app)
