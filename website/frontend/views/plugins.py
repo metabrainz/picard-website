58cb01700ae6cd8d1d8d585861c75ef775bc0b3f
Laurent Monin <zas@norz.org>
2015-09-09 10:30:21 +0200

    Sort plugins by name

--- website/frontend/views/plugins.py
+++ website/frontend/views/plugins.py
@@ -1,17 +1,19 @@
+from collections import OrderedDict
 import os
 import json
 from urllib import urlopen
 from flask import current_app, Blueprint, render_template
-
 
 plugins_bp = Blueprint('plugins', __name__)
 
 
 @plugins_bp.route('/')
 def show_plugins():
-    # TODO : sorting
+    ordered_plugins = OrderedDict()
     plugins_json_file = os.path.join(
         current_app.config['PLUGINS_REPOSITORY'], "plugins.json")
     with open(plugins_json_file, "r") as fp:
-        plugins = json.loads(fp.read().decode("utf-8"))
-    return render_template('plugins.html', plugins=plugins['plugins'])
+        plugins = json.loads(fp.read().decode("utf-8"))['plugins']
+        for key in sorted(plugins, key=lambda k: plugins[k]['name'].lower()):
+            ordered_plugins[key] = plugins[key]
+    return render_template('plugins.html', plugins=ordered_plugins)
