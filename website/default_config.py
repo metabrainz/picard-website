2d0fc47b88e068ac2547eb28998e258d253c4712
Philipp Wolfer <ph.wolfer@gmail.com>
2021-03-30 12:06:16 +0200

    Picard release 2.6

--- website/default_config.py
+++ website/default_config.py
@@ -24,18 +24,20 @@ PLUGIN_VERSIONS = {
 # The Picard developers have standardized on using only 'dev' or 'final' as the str_type segment of the version tuple.
 PICARD_VERSIONS = {
     'stable': {
-        'tag': '2.5.6',
-        'version': (2, 5, 6, 'final', 0),
+        'tag': '2.6',
+        'version': (2, 6, 0, 'final', 0),
         'urls': {
             'download': 'https://picard.musicbrainz.org/',
             'changelog': 'https://picard.musicbrainz.org/changelog/',
         },
         'win_size': '30',
-        'win_hash': 'c1f1ab6e4f4f0acc4c2f26c4c6270a99',
+        'win_hash': 'b671ea57574198c4f0db94386d5680ee',
         'win_portable_size': '43',
-        'win_portable_hash': 'eb6c66e6473a4fa33cff0288f1717d80',
-        'mac_size': '31',
-        'mac_hash': '42823e860d47a2bf67b7caff7f9851f4',
+        'win_portable_hash': '3f465f2d43f6bcd436285ea2e0002e5e',
+        'mac_10_12_size': '32',
+        'mac_10_12_hash': '2d2208a60735532891ddf0e3df03fc0e',
+        'mac_size': '34',
+        'mac_hash': 'fce5e65d885549e1dfba940f70d26cc4',
         'linux_size': '4.1',
         'linux_hash': '-',
     },
