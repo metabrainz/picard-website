b93c0c23c7a9664c8346bbd0758959007130da86
Philipp Wolfer <ph.wolfer@gmail.com>
2025-12-19 15:49:59 +0100

    Update parsing and re-formatting the changelog
    Adjust for formatting changes in Picard repo

--- website/frontend/views/changelog.py
+++ website/frontend/views/changelog.py
@@ -11,13 +11,17 @@ version_header = '<h3 id="release-{0}">Version <strong>{0}</strong> <span>{1}</s
 
 class ChangelogRenderer(mistune.HTMLRenderer):
     def heading(self, text, level, **attrs):
+        # Ignore h1 headline
         if level == 1:
+            return ""
+        # Reformat h2 version headlines
+        elif level == 2:
             match = re_version.match(text)
             if match:
                 version = match.group(1)
                 date = match.group(2).replace("xxxx-xx-xx", "Yet to be released")
                 return version_header.format(version, date)
-        return super().heading(text, level + 2)
+        return super().heading(text, level + 1)
 
 
 renderer = ChangelogRenderer()
