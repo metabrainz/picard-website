f4f1096416f9f4350165e3bea3c8265b78ebdbab
Laurent Monin <github@norz.org>
2026-02-21 11:40:05 +0100

    plugins-generate: skip version 3

--- plugins-generate.py
+++ plugins-generate.py
@@ -12,6 +12,11 @@ config = website.frontend.create_app().config
 versions = [z['title'] for z in config['PLUGIN_VERSIONS'].values()]
 build_dir = config['PLUGINS_BUILD_DIR']
 for version in versions:
+    # Only generate for v1 and v2 - v3+ use remote registry
+    if version not in ('1.0', '2.0'):
+        print(f"Skipping version {version} (uses remote registry)")
+        continue
+
     print(build_dir, version)
     try:
         generate_plugins(build_dir, version)
