35d569692aa8b1fc2d6d0f4be14d472fb5dfacea
Laurent Monin <github@norz.org>
2026-02-23 12:52:29 +0100

    Improve string formatting readability using parentheses

--- website/frontend/views/api_test.py
+++ website/frontend/views/api_test.py
@@ -8,8 +8,9 @@ from website.frontend.testing import FrontendTestCase
 _MESSAGES = {
     'plugin_not_found': 'Plugin not found.',
     'missing_api_version': 'No API version specified',
-    'invalid_endpoint': 'The endpoints currently available for this api version'
-    ' are /api/v1/plugins and /api/v1/download',
+    'invalid_endpoint': (
+        'The endpoints currently available for this api version are /api/v1/plugins and /api/v1/download'
+    ),
     'missing_id': 'Plugin id not specified.',
     'download_usage': 'Correct usage: /api/v1/download?id=<id>',
 }
